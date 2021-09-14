aa9696d413900dc5715d76ce0f6f784901d75dbd
MarinaSev <pawpawsap@gmail.com>
2021-09-14 15:14:41 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 40_000;
+        int rubForBonus = 20;
+        int miles = ticketPrice / rubForBonus;
+        System.out.println(miles);
+    }
+}
